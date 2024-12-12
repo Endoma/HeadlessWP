2cd4843bc6be259ec5fdd2dfdf7465d3c421a77f
matt <4mattd@gmail.com>
2024-12-12 21:30:52 +0100

    testing getoption

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 if (!is_user_logged_in()) {
     $url = get_option('headlesswp_api_url', '');
     //echo $value;
-    header( "Location: $url" );
+    header( "Location:" . $url);
 }
 else {
     echo '<a href="/wp-admin" class="button">Go to Admin</a>';
-}getoption
+}
